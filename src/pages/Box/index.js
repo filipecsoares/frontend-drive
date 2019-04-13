321b3b66168981bd90b8dca5b9f5d6c2c03c4711
Filipe Soares <filipecarlossoares@gmail.com>
2019-04-13 14:31:46 -0300

    Create Main page

---
+++ src/pages/Box/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg';
+import './styles.css';
+
+export default class Box extends Component {
+  render() {
+    return <div />;
+  }
+}
